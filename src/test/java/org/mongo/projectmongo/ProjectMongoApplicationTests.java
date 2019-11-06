0820561e8a3e2c65adfea85caf5945a8ae2e2ed4
BartoszRedkowiak <b.redkowiak@gmail.com>
2019-11-06 13:50:56 +0100

    Initial Commit

---
+++ src/test/java/org/mongo/projectmongo/ProjectMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mongo.projectmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
